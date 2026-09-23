8bf87104cab24741709299dacffb9b31d60da0de
saihariharan017 <saihariharan112005@gmail.com>
2026-09-23 21:34:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ if user_input:
         with st.spinner("Thinking..."):
             try:
                 response = client.models.generate_content(
-                    model="gemini-2.5-flash",
+                   model="gemini-3.6-flash",
                     contents=conversation
                 )
 
